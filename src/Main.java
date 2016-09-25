d90def516dd83259aac421bf198aca96b1166f57
neek1337 <vdo1337@gmail.com>
2016-09-25 12:55:43 +0300

    25

--- src/Main.java
+++ src/Main.java
@@ -6,48 +6,39 @@ public class Main implements Runnable {
     private static Scanner scanner;
     private static BufferedReader in;
     private static PrintWriter out;
-    private static Thread thread;
+    private static boolean stoped = false;
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Добро пожаловать!");
         scanner = new Scanner(System.in);
         //создаем отдельный поток для чтения входящих сообщений от сервера
         while (true) {
-            String[] line = scanner.nextLine().split(" ");
-            switch (line[0].toLowerCase()) {
-                case "connect":
-                    try {
-                        //проверяем кол-во введеных параметров для метода connect
-                        if (line.length != 3) {
-                            System.out.println("Команда connect имеет следующий вид: connect server_name[:port] UserName");
-                            break;
-                        }
-                        new Main().connect(line[1], line[2]);
-                        return;
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    new Main().connect("localhost:4444", "Name");
+                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-                default:
-                    System.out.println("Введена неврная команда! Доступные команды: onnect server_name[:port] UserName и exit");
-            }
+
         }
     }
 
     public void run() {
-        String responseLine = null;
+        String responseLine;
         try {
             // "прослушиваем" сообщения от сервера
             while ((responseLine = in.readLine()) != null) {
-                    System.out.println(responseLine);
+                System.out.println(responseLine);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Ошибка соединения с сервером. Для выхода нажмите Enter" +
+                    "");
+            stoped = true;
         }
     }
 
-    public void connect(String ip, String name) throws IOException {
-        String server = null, port = null;
+    private void connect(String ip, String name) throws IOException {
+        String server, port;
         if (ip.contains(":")) {
             String[] splitedIp = ip.split(":");
             if (splitedIp.length != 2) {
@@ -60,7 +51,7 @@ public class Main implements Runnable {
             server = ip;
             port = "4444";
         }
-        Socket fromserver = null;
+        Socket fromserver;
         //отлавливаем исключение, возникающее при попытке подключиться к серверу по неправильному адресу.
         try {
             fromserver = new Socket(server, Integer.parseInt(port));
@@ -80,19 +71,16 @@ public class Main implements Runnable {
             String responseLine = in.readLine();
             System.out.println(responseLine);
             // если подключение прошло успешно, создаем поток, "прослушивающий" сообщения от сервера
-            if (responseLine.startsWith("Количество подключенных пользователей:")) {
-                thread = new Thread(new Main());
+                Thread thread = new Thread(new Main());
                 thread.start();
                 while (scanner.hasNextLine()) {
-                    line = scanner.nextLine();
-                    if (line.equalsIgnoreCase("quit")) {
-                        thread.stop();
-                        out.println(line);
-                        break;
+                    if (stoped) {
+                        return;
                     }
+                    line = scanner.nextLine();
                     out.println(line);
                 }
-            }
+
         } finally {
             out.close();
             in.close();
